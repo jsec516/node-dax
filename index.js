d9df5fa503de678c711f4d7964efee4724f272f5
MD. Jahidul Islam <jsec516@gmail.com>
2017-06-17 15:06:37 +0600

    update

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 var java = require("java");
 java.classpath.push('./lib/jfiles');
-var TryDax = java.import('TryDax');
-var dax = new TryDax();
+var DynamoDB = java.import('com.amazonaws.services.dynamodbv2.document.DynamoDB');
+var TryDaxHelper = java.import('TryDaxHelper');
+var TryDaxTests = java.import('TryDaxTests');
+var helper = new TryDaxHelper();
+var tests = new TryDaxTests();
+var ddbClient = helper.getDynamoDBClientSync();
+var daxClient = null;
+if (process.env.dax_client) {
+    console.log(process.env.dax_client);
+    daxClient = helper.getDaxClientSync(process.env.dax_client);
+}
+var tableName = "TryDaxTable";
+
+console.log("Creating table...");
+helper.createTable(tableName, ddbClient);
+console.log("Populating table...");
+helper.writeDataSync(tableName, ddbClient, 10, 10);
+
+console.log('done with processing ...');
